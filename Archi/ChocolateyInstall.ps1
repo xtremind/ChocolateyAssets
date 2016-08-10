222e24578dcc575d68c08dc045771d6474520f7f
Filip De Vos <filipdevoskls@gmail.com>
2016-08-10 09:56:24 -0400

    Add support for checksums
    In version 3.2.2.1 the chocolatey package will use a checksum to verify
    the integrity of the downloaded installation package.

--- Archi/ChocolateyInstall.ps1
+++ Archi/ChocolateyInstall.ps1
@@ -1,6 +1,10 @@
-$name = "archi"
-$url = "http://archimatetool.com/downloads/latest/Archi-win32-Setup-3.3.2.exe"
-$kind = "EXE"
-$silent = "/VERYSILENT"
+$packageArgs = @{
+  packageName   = "archi"
+  fileType      = 'EXE'
+  url           = "http://archimatetool.com/downloads/latest/Archi-win32-Setup-3.3.2.exe" 
+  silentArgs    = "/VERYSILENT"
+  checksum      = "44C86122F02A7C60D132968E288ADA4908D2B26B91B20E5C19EDB2A5CB800FB8"
+  checksumType  = 'sha256'
+}
 
-Install-ChocolateyPackage $name $kind $silent $url
+Install-ChocolateyPackage @packageArgs
