893642574b3874e166198c7bf5bc1aae2eadaa67
Filip De Vos <filipdevoskls@gmail.com>
2017-05-09 12:40:36 -0400

    Fix broken download link.

--- Archi/ChocolateyInstall.ps1
+++ Archi/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageArgs = @{
   packageName   = "archi"
   fileType      = 'EXE'
-  url           = "http://archimatetool.com/downloads/latest/Archi-win32-Setup-3.3.2.exe" 
+  url           = "https://archimatetool.com/downloads/release/v3/Archi-win32-Setup-3.3.2.exe" 
   silentArgs    = "/VERYSILENT"
   checksum      = "44C86122F02A7C60D132968E288ADA4908D2B26B91B20E5C19EDB2A5CB800FB8"
   checksumType  = 'sha256'
