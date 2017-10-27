a1572d072b58733e3b15b02f9fcd9d44236f8a64
Alex Ivanov <amivanoff@gmail.com>
2017-10-27 19:47:31 +0300

    Update Archi to 64bit version

--- Archi/ChocolateyInstall.ps1
+++ Archi/ChocolateyInstall.ps1
@@ -1,7 +1,7 @@
 $packageArgs = @{
   packageName   = "archi"
   fileType      = 'EXE'
-  url           = "https://archimatetool.com/downloads/release/v4/Archi-Windows-Setup-4.0.3.exe" 
+  url           = "https://archimatetool.com/downloads/release/v4/Archi-Win64-Setup-4.0.3.exe" 
   silentArgs    = "/VERYSILENT"
   checksum      = "7f1a34d92dfd13c12ede894ab6fd86479bfc4cc29260e8c614e465e8c20028a9"
   checksumType  = 'sha256'
