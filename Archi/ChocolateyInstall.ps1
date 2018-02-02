0e08e162c0f0ccac613e0dbc5a4367ebbf264d33
Filip De Vos <filipdevoskls@gmail.com>
2018-02-02 17:19:55 -0500

    Update package to archi 4.2

--- Archi/ChocolateyInstall.ps1
+++ Archi/ChocolateyInstall.ps1
@@ -1,9 +1,9 @@
 $packageArgs = @{
   packageName   = "archi"
   fileType      = 'EXE'
-  url           = "https://archimatetool.com/downloads/release/v4/Archi-Win64-Setup-4.0.3.exe" 
+  url           = "https://archimatetool.com/downloads/release/Archi-Win64-Setup-4.2.exe" 
   silentArgs    = "/VERYSILENT"
-  checksum      = "7f1a34d92dfd13c12ede894ab6fd86479bfc4cc29260e8c614e465e8c20028a9"
+  checksum      = "8D8E02A0AE1676FBAFDC5DB37CAFC4670A4BCBF9AE0641AA5FB78C801904EAF7"
   checksumType  = 'sha256'
 }
 
