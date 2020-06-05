60927bc9aaadd07511ed9e6d8fda44a7203581a6
Guillaume Da Graça <guillaume.da-graca@capgemini.com>
2020-06-05 09:20:45 +0200

    upgrading to 4.6.0

--- Archi/tools/ChocolateyInstall.ps1
+++ Archi/tools/ChocolateyInstall.ps1
@@ -1,21 +1,17 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.archimatetool.com/downloads/rel-42/Archi-Win32-Setup-4.2.exe'
-$url64      = 'https://www.archimatetool.com/downloads/rel-42/Archi-Win64-Setup-4.2.exe'
+$url64      = 'https://www.archimatetool.com/downloads/19ef8fac18e/Archi-Win64-Setup-4.6.0.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'EXE'
-  url           = $url
   url64bit      = $url64
 
   softwareName  = 'archi'
 
-  checksum      = '39eb87626a26ad01aac578143cb2037e'
-  checksumType  = 'md5'
-  checksum64    = '29e57834f6631b3994bebce839b4a4de'
+  checksum64    = '228ffb9280e8d5d4fd20cda4482fc9ac'
   checksumType64= 'md5'
 
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
